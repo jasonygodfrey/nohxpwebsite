3333f3a19a831ce43bbc76df68d28f84ded1bc8d
Jason <83245646+jasonygodfrey@users.noreply.github.com>
2023-04-27 16:59:42 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import Image from 'next/image';
 
 const Index = () => {
   return (
     <div>
       <h1>Hello, Next.js!</h1>
+      <div style={{ position: 'relative', width: '100%', height: 'auto' }}>
+        <Image
+          src="/template.png"
+          alt="My Picture"
+          layout="responsive"
+          width={500} // Replace with the original image width
+          height={300} // Replace with the original image height
+        />
+      </div>
     </div>
   );
 };
