0e012011bad2dc97726e54d9424ae36edb8b8902
Jason <83245646+jasonygodfrey@users.noreply.github.com>
2023-05-01 01:20:10 +0000

    4-30

--- pages/index.js
+++ pages/index.js
@@ -14,36 +14,54 @@ const Index = () => {
           layout="responsive"
         />
       </div>
+      <div className={styles.title}>
+        <Image
+          src="/NOHXPETITLE.png"
+          alt="Title"
+          width={500}
+          height={100}
+          layout="responsive"
+        />
+      </div>
       <div className={styles.buttonRowContainer}>
         <div className={styles.imageContainer}>
         <img
-            src="/IMG_0069-4.png"
+            src="/IMG_0069-3.png"
             alt="Image"
             width={500}
             height={500}
             className={`${styles.image} ${styles.centerImage}`} // Add the centerImage class here
           />
         </div>
-        <div className={styles.buttonRow}>
-          <button className={`${styles.button} ${styles.bottomButton}`}>Button 1A</button>
-          <button className={`${styles.button} ${styles.bottomButton}`}>Button 2A</button>
-          <button className={`${styles.button} ${styles.bottomButton}`}>Button 3A</button>
-          <button className={`${styles.button} ${styles.bottomButton}`}>Button 4A</button>
-          <button className={`${styles.button} ${styles.bottomButton}`}>Button 5A</button>
-          <button className={`${styles.button} ${styles.bottomButton}`}>Button 6A</button>
-        </div>
+        {
+  /* 
+  <div className={styles.buttonRow}>
+    <button className={`${styles.button} ${styles.bottomButton}`}>Button 1A</button>
+    <button className={`${styles.button} ${styles.bottomButton}`}>Button 2A</button>
+    <button className={`${styles.button} ${styles.bottomButton}`}>Button 3A</button>
+    <button className={`${styles.button} ${styles.bottomButton}`}>Button 4A</button>
+    <button className={`${styles.button} ${styles.bottomButton}`}>Button 5A</button>
+    <button className={`${styles.button} ${styles.bottomButton}`}>Button 6A</button>
+  </div>
+  */
+}
+
       </div>
       <aside className={styles.sidebar}>
-        <button className={styles.sidebarButton}>Button 1B</button>
-        <button className={styles.sidebarButton}>Button 2B</button>
-        <button className={styles.sidebarButton}>Button 3B</button>
-        <button className={styles.sidebarButton}>Button 4B</button>
-        <button className={styles.sidebarButton}>Button 5B</button>
-        <button className={styles.sidebarButton}>Button 6B</button>
-        <button className={styles.sidebarButton}>Button 7B</button>
-        <button className={styles.sidebarButton}>Button 8B</button>
-        <button className={styles.sidebarButton}>Button 9B</button>
-        <button className={styles.sidebarButton}>Button 10B</button>
+      <a
+  href="https://www.instagram.com/nohxpeco/"
+  target="_blank"
+  rel="noopener noreferrer"
+  className={`${styles.sidebarLink} ${styles.instagramLink}`} // Add the instagramLink class to the <a> tag
+>
+  <button className={`${styles.sidebarButton} ${styles.button1b}`}>INSTAGRAM</button>
+</a>
+
+
+        <button className={`${styles.sidebarButton} ${styles.button4b}`}>TWITTER</button>
+
+        <button className={`${styles.sidebarButton} ${styles.button9b}`}>TBA</button>
+
       </aside>
     </div>
   );
